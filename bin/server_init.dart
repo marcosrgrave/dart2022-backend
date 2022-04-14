d193775a1a7a8ea8ed12487c91fed54162d9d85f
note_m1 <marcos.r.grave@gmail.com>
2022-04-14 20:15:15 -0300

    moved backend basics files to new folder

--- bin/server_init.dart
+++
@@ -1,8 +0,0 @@
-import 'package:shelf/shelf.dart';
-import 'package:shelf/shelf_io.dart' as shelf_io;
-
-void main() async {
-  final server = await shelf_io.serve(
-      (request) => Response(200, body: "Server Test"), "localhost", 8080);
-  print("Server Iniciated");
-}
