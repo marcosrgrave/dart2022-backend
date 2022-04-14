e266d3907550066f7ed1286b26e2d14abe1c5ae9
note_m1 <marcos.r.grave@gmail.com>
2022-04-14 00:17:01 -0300

    http post basics

--- NOTES.md
+++ NOTES.md
@@ -1,15 +1,31 @@
 ## Packages
 
-### shelf.dart
+### **shelf.dart**
 
 Has everything available to create de server, even Request and Response methods.
 
-### shelf_router.dart
+### **shelf_router.dart**
 
 Makes the task to create routes easier and simpler.
 
-## Query Parameters
+## HTTP
+
+### **Query Parameters**
 
 They are parameters attached to the url, like the name of the home page.
 
 Is extremely important to avoid using sensitive information as query parameters.
+
+### **Response Status Code**
+
+Informational responses (100–199)
+
+Successful responses (200–299)
+
+Redirection messages (300–399)
+
+Client error responses (400–499)
+
+Server error responses (500–599)
+
+https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
--- bin/my_server_handler.dart
+++ bin/my_server_handler.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
@@ -5,7 +7,7 @@ class MyServerHandler {
   Handler get handler {
     final rout = Router();
 
-    // get basics
+    // GET basics
     rout.get("/", (Request req) {
       return Response(200, body: "Main Route");
     });
@@ -23,6 +25,25 @@ class MyServerHandler {
       String? firstQueryInput = req.url.queryParameters["fqi"];
       String? intNumber = req.url.queryParameters["intNumber"];
       return Response(200, body: "Query input is: $firstQueryInput $intNumber");
+    });
+
+    // POST basics
+    rout.post("/login", (Request req) async {
+      // get values from application body
+      String jsonStringValues = await req.readAsString();
+      Map jsonMapValues = jsonDecode(jsonStringValues);
+
+      String _user = "marcosrgrave";
+      String _password = "dart2022-backend";
+
+      String? userInput = jsonMapValues["user"];
+      String? passwordInput = jsonMapValues["password"];
+
+      if (userInput == _user && passwordInput == _password) {
+        return Response.ok("Congratulations! You are logged in!");
+      } else {
+        return Response.forbidden("Invalid Access. Please, try again.");
+      }
     });
 
     return rout;
