fac9dc9392dbc62bbca2cbd8e8be23099661bd0a
note_m1 <marcos.r.grave@gmail.com>
2022-04-24 15:33:35 -0300

    created generic and article services

---
+++ bin/services/article_service.dart
@@ -0,0 +1,28 @@
+import '../models/article_model.dart';
+import 'generic_service.dart';
+
+class ArticleService implements GenericService<ArticleModel> {
+  @override
+  bool delete(ArticleModel value) {
+    // TODO: implement delete
+    throw UnimplementedError();
+  }
+
+  @override
+  List<ArticleModel> getAll() {
+    // TODO: implement getAll
+    throw UnimplementedError();
+  }
+
+  @override
+  ArticleModel getObject(int id) {
+    // TODO: implement getObject
+    throw UnimplementedError();
+  }
+
+  @override
+  bool save(ArticleModel value) {
+    // TODO: implement save
+    throw UnimplementedError();
+  }
+}
---
+++ bin/services/generic_service.dart
@@ -0,0 +1,6 @@
+abstract class GenericService<Type> {
+  Type getObject(int id);
+  List<Type> getAll();
+  bool save(Type value);
+  bool delete(Type value);
+}
