dbba70a17963f6d7e5cc23b6cdf0aac5abe28269
note_m1 <marcos.r.grave@gmail.com>
2022-04-24 15:04:54 -0300

    blog API endpoints

--- bin/api/blog.dart
+++ bin/api/blog.dart
@@ -5,8 +5,28 @@ class Blog {
   Handler get blogHandler {
     Router rout = Router();
 
+    // Current blog articles
     rout.get("/blog", (Request req) {
       return Response.ok("Blog accessed.");
+    });
+
+    // Adding a new article
+    rout.post("/blog", (Request req) {
+      return Response.ok("New article added.");
+    });
+
+    // Updating an article
+    // e.g., /blog?id=12345
+    rout.put("/blog", (Request req) {
+      String? id = req.url.queryParameters["id"];
+      return Response.ok("Article of id: $id has been updated.");
+    });
+
+    // Deleting an article
+    // e.g., /blog?id=12345
+    rout.delete("/blog", (Request req) {
+      String? id = req.url.queryParameters["id"];
+      return Response.ok("Article of id $id has been deleted.");
     });
 
     return rout;
